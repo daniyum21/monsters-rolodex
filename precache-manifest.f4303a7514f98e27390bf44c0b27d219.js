751dc40347340293d2e08d59ffbb6cc1e973225a
Daniel Tangente <daniyum21@gmail.com>
2020-01-26 00:38:03 -0800

    Updates

---
+++ precache-manifest.f4303a7514f98e27390bf44c0b27d219.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b32d61609730acccfbb4238831cb97c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ecce1337434126777d56",
+    "url": "/monsters-rolodex/static/css/main.86620762.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ecce1337434126777d56",
+    "url": "/monsters-rolodex/static/js/main.ba42ad5b.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
